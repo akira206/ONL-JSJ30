4993756af72d7648e8d913a587a734bd4eff9222
akira2kar6 <109467738+akira2kar6@users.noreply.github.com>
2023-03-12 21:28:59 +0700

    buoi1

---
+++ Buổi 1/abc.js
@@ -0,0 +1,22 @@
+let student = [
+    {
+        name : "VNA",
+        age : 16,
+        course : ["mindX 01", "mindX 02", "mindX 03"]
+    },
+    {
+        name : "VNA",
+        age : 18,
+        course : ["mindX 01", "mindX 02", "mindX 03"]
+    },
+    {
+        name : "VNA",
+        age : 17,
+        course : ["mindX 01", "mindX 02", "mindX 03"]
+    },
+    {
+        name : "VNA",
+        age : 15,
+        course : ["mindX 01", "mindX 02", "mindX 03"]
+    }
+]
